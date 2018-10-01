2f996df1de7c2f343a5ee81f161f4274e102f0c8
inneroot <hakooplay@gmail.com>
2018-10-01 21:29:17 +0000

    first commit

---
+++ app/javascript/day.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="day">
+    <h1>{{ mday }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["message", "mday"],
+}
+</script>
+
+<style scoped>
+.day {
+    display: block;
+    flex-shrink: 0;
+    height: 150px;
+    width: 200px;
+    border: 1px solid black;
+}
+</style>
